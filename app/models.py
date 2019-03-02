d8587cb0a4977e0dc60f5a3aba3d754167bc036e
a649414476 <a649414476@gmail.com>
2019-03-02 15:48:09 +0800

    user hashed password and test added User shell context made

--- app/models.py
+++ app/models.py
@@ -1,18 +1,29 @@
 from app import db
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 class User(db.Model):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     name = db.Column(db.String(20), nullable=False)
-    password = db.Column(db.String(20), nullable=False)
+    password_hash = db.Column(db.String(128))
     admin = db.Column(db.Boolean, nullable=False, default=False)
-    com = db.relationship('Comment', backref='user')
+    com = db.relationship('Comment', backref='users')
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return '<user %r>' % (self.name)
+        return '<user %r>' % self.name
 
 
 class Txt(db.Model):
@@ -25,7 +36,7 @@ class Txt(db.Model):
     com = db.relationship('Comment', backref='txt')
 
     def __repr__(self):
-        return '<activity %r>' % (self.title)
+        return '<activity %r>' % self.title
 
 
 class Comment(db.Model):
@@ -34,7 +45,7 @@ class Comment(db.Model):
     txtid = db.Column(db.Integer, db.ForeignKey('txt.id'))
     cont = db.Column(db.String(160))
     time = db.Column(db.Text(20), nullable=False)
-    user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def __repr__(self):
-        return '<com %r>' % (self.id)
+        return '<com %r>' % self.id
