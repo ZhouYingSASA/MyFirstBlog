117674a7085fd4085136e88a48e7ae42fa92e340
a649414476 <a649414476@gmail.com>
2019-03-06 18:32:56 +0800

    login/logout button added.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, Regexp, ValidationError
+from ..models import Users
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +9,23 @@ class LoginForm(FlaskForm):
     password = PasswordField('输入密码', validators=[DataRequired()])
     remember_me = BooleanField('保持登陆')
     submit = SubmitField('登陆')
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('邮箱地址', validators=[DataRequired(), Length(1, 64), Email()])
+    username = StringField('用户名', validators=[DataRequired(), Length(1, 64),
+                                              Regexp('[\u4E00-\u9FD5a-zA-Z0-9_.]*$', 0,
+                                                     '仅支持常见汉字、英文大小写、数字、点及下划线命名。')])
+    password = PasswordField('输入密码', validators=[DataRequired()])
+    password2 = PasswordField('再次输入密码', validators=[DataRequired()])
+    submit = SubmitField('注册')
+
+    @staticmethod
+    def validate_email(field):
+        if Users.query.filter_by(email=field.data).first():
+            raise ValidationError('该邮箱已注册！')
+
+    @staticmethod
+    def validate_username(field):
+        if Users.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名已存在！')
